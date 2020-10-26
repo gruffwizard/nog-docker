0611c33b0b76e04d5338006038c56b338e2bc874
Steve Poole <spoole167@googlemail.com>
2020-10-26 19:36:04 +0000

    aligned images with nog cli to use envars for actions

--- nog.sh
+++ nog.sh
@@ -1,18 +1,59 @@
 #!/bin/bash
-echo "Nog Runner"
-echo "Current Dir :$PWD"
-echo "Maven       :$M2_HOME"
 
-case "$1" in
+verbose=false
+ide=false
 
-edit)
-  cd /home/nog/tools/theia && yarn start /home/nog/src  &
-  ;;
-dev)
+src="/home/nog/src"
+
+echo -e "\e[97mNog Runner\e[39m"
+
+if [[ ! -z "${NOG_VERBOSE}" ]]; then
+  verbose=true
+fi
+
+if $verbose; then
+    echo "NOG_IDE    = $NOG_START_IDE"
+    echo "QUICKSTART = $NOG_QUICKSTART"
+    git --version
+fi
+
+
+if [[ ! -z "${NOG_QUICKSTART}" ]]; then
+  quickstart=${NOG_QUICKSTART}
+
+  if [ ! -d /home/nog/quarkus-quickstarts ]; then
+    echo -e "\e[31mmain quickstart directory not present\e[0m"
+    exit -1
+  fi
+
+  # check to see if there is a .git repo present
+  # if not shallow clone the quick starts
+
+  if [ ! -d /home/nog/quarkus-quickstarts/.git ]; then
+     cd ~/quarkus-quickstarts/ && git clone --depth 1 https://github.com/quarkusio/quarkus-quickstarts .
+  fi
+
+  if [ ! -d /home/nog/quarkus-quickstarts/$quickstart ]; then
+    echo -e "\e[31mquickstart sample $quickstart is not present\e[0m"
+    exit -1
+  fi
+
+  # copy over the specified quickstart into a subdir of src
+
+  cp -r /home/nog/quarkus-quickstarts/$quickstart $src/$quickstart
+  src=$src/$quickstart
+
+fi
+
+if [[ ! -z "${NOG_START_IDE}" ]]; then
+  cd /home/nog/tools/theia && yarn start $src  &
+fi
+
+cd $src
+
+  if [ ! -f "./pom.xml" ]; then
+    echo  -e "\e[31mno pom.xml file present\e[0m"
+    exit -1
+  fi
+
   mvn compile quarkus:dev
-  ;;
-quickstart)
-  echo "quickstart" $2
-  git status
-  ;;
-esac
